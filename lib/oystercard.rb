9dae2588e6ff245556107debf249a9941593c34d
Lewis Jones <lewis.jones1997@protonmail.com>
2021-09-27 14:54:27 +0100

    refactor(*): use constants and interpolation, restructure tests

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,18 +1,19 @@
 class Oystercard
     attr_reader :balance
+    LIMIT = 90
 
     def initialize 
         @balance = 0
     end
 
     def top_up(value)
-        raise "Limit of £90 exceeded" if is_over_limit?(value)
+        raise "Limit of £#{LIMIT} exceeded" if is_over_limit?(value)
         @balance += value
     end
 
     private
 
     def is_over_limit?(value)
-        @balance + value > 90
+        @balance + value > LIMIT
     end
 end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,16 +1,21 @@
 require 'oystercard'
 
 describe Oystercard do
-    it "an onystercard will be initilized with a balance of zero" do
-        expect(subject.balance).to eq 0
-    end
 
+  describe "#initialize" do 
+    it "an onystercard will be initilized with a balance of zero" do
+      expect(subject.balance).to eq 0
+    end
+  end
+
+  describe "#top_up" do
     it "balance will be increased when the top_up method is invoked" do
-        subject.top_up(5)
-        expect(subject.balance).to eq 5
+      subject.top_up(5)
+      expect(subject.balance).to eq 5
     end
 
     it "the balance cannot be increased past £90" do       
         expect{subject.top_up(95)}.to raise_error "Limit of £90 exceeded"
     end
+  end
 end
