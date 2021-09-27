52100b1b76e026491ec8f17e9a71d5eb51793a83
Stella Martin <stellamartin222@gmail.com>
2021-09-27 14:50:57 +0100

    Chapter 6 completed

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,3 +1,18 @@
 class Oystercard
+    attr_reader :balance
 
+    def initialize 
+        @balance = 0
+    end
+
+    def top_up(value)
+        raise "Limit of £90 exceeded" if is_over_limit?(value)
+        @balance += value
+    end
+
+    private
+
+    def is_over_limit?(value)
+        @balance + value > 90
+    end
 end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,5 +1,16 @@
 require 'oystercard'
 
 describe Oystercard do
-  
+    it "an onystercard will be initilized with a balance of zero" do
+        expect(subject.balance).to eq 0
+    end
+
+    it "balance will be increased when the top_up method is invoked" do
+        subject.top_up(5)
+        expect(subject.balance).to eq 5
+    end
+
+    it "the balance cannot be increased past £90" do       
+        expect{subject.top_up(95)}.to raise_error "Limit of £90 exceeded"
+    end
 end
