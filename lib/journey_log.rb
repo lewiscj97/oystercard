4013d3f6b6cee8e52d30d393b86cb6be471e0ac5
Lewis Jones <lewis.jones1997@protonmail.com>
2021-09-28 16:49:46 +0100

    feat(*): implement journeylog class

--- lib/journey_log.rb
+++ lib/journey_log.rb
@@ -1,14 +1,25 @@
 require_relative "journey"
 
 class JourneyLog
-    attr_reader :journey_class
+    attr_reader :current_journey
 
     def initialize(journey = Journey)
         @journey_class = journey
+        @current_journey
+        @journeys = []
     end
 
     def start(entry_station)
-        journey = @journey.new
-        journey.touch_in(entry_station)
+        @current_journey = @journey_class.new
+        @current_journey.touch_in(entry_station)
+    end
+
+    def finish(exit_station)
+        @current_journey.touch_out(exit_station)
+        @journeys << @current_journey
+    end
+
+    def journeys
+        @journeys.map(&:clone)
     end
 end
--- spec/journey_log_spec.rb
+++ spec/journey_log_spec.rb
@@ -2,21 +2,31 @@ require "journey_log"
 
 describe JourneyLog do
 
-    let(:journey) {double :journey}
     let(:journey) {double :journey, entry_station: lea_green, exit_station: wavertree, fare: 1}
     let(:lea_green) {double :station}
     let(:wavertree) {double :station}
 
-    describe "#initialize" do
-        it "will be inialized with a journey" do
-            expect(subject.journey_class).to eq Journey
-        end
-    end
-
     describe "#start" do
         it "will start a journey with an entry station" do
             subject.start(lea_green)
-            expect(subject.journey_class.entry_station).to eq lea_green
+            expect(subject.current_journey.entry_station).to eq lea_green
+        end
+    end
+
+    describe "#finish" do
+        it "adds an exit station to the current journey" do
+            subject.start(lea_green)
+            subject.finish(wavertree)
+            expect(subject.current_journey.exit_station).to eq wavertree
+        end
+    end
+
+    describe "#journeys" do
+        it "shows all previous journeys" do
+            subject.start(lea_green)
+            subject.finish(wavertree)
+            expect(subject.journeys[0].entry_station).to eq lea_green
+            expect(subject.journeys[0].exit_station).to eq wavertree
         end
     end
 end
